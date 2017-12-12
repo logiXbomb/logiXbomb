b26125b372cbe1f3093682c381399232e1507277
Seth Lowie <slowie@powerchordsystem.com>
2017-12-12 15:10:46 -0500

    clean repo

--- src/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Route } from 'react-router';
-import App from './components/App.jsx';
-
-const routes = (
-  <Route path="/" component={App} />
-);
-
-export default routes;
